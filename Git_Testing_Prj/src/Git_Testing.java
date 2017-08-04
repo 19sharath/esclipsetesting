53b409e517b490fc1dcfe1b315ad0d14f56eab0b
19sharath <sharath1122@gmail.com>
2017-08-04 14:27:00 +0530

    checking it

--- Git_Testing_Prj/src/Git_Testing.java
+++ Git_Testing_Prj/src/Git_Testing.java
@@ -4,10 +4,17 @@ public class Git_Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testing Git");
-		
-		System.out.println("Checking the build");
-
+	boolean m;
+	m = true;
+	if(m = false) 
+	
+	{
+				System.out.println("Testing Git");
+				System.out.println("Testing it");
 	}
+//	else
+//		System.out.println("Checking the build");
+//
+}
 
 }
